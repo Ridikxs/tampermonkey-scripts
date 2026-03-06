5ce1a7abcfbc6b293cabf079502c9d1306000ad5
Ridikxs <118582927+Ridikxs@users.noreply.github.com>
2026-03-06 15:26:59 +0400

    обнова ссылок на скачивание 2.1

--- LiveChat Timer.user.js
+++ LiveChat Timer.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         LiveChat Timer
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      2.1
 // @description  Исправлено смещение таймера при переходе в настройки.
 // @author       Calvin/River
 // @match        https://my.livechatinc.com/*
-// @updateURL    https://raw.githubusercontent.com/Ridikxs/tampermonkey-scripts/main/LiveChatTimer.user.js
-// @downloadURL  https://raw.githubusercontent.com/Ridikxs/tampermonkey-scripts/main/LiveChatTimer.user.js
+// @updateURL    https://raw.githubusercontent.com/Ridikxs/tampermonkey-scripts/main/LiveChat%20Timer.user.js
+// @downloadURL  https://raw.githubusercontent.com/Ridikxs/tampermonkey-scripts/main/LiveChat%20Timer.user.js
 // @grant        none
 // ==/UserScript==
 
@@ -226,3 +226,4 @@
     injectButtons();
 
 })();
+
